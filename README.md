2a6b57810a86c21e28dcb1f13fc25d604695ea3c
Spyder <owsley.wood@gmail.com>
2024-02-25 02:55:55 -0800

    Created comment outline.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # system-init
+
+Will eventually contian the files and scripts necessary
+to initialize a complete environment for development.
+
---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+# Configure Shell
+# Add Aliases
+# Install Oh my Zsh
+
+# Install Programs
+# C/CPP
+# GCC
+# Make
+# CMake
+# Build2
+# Optional Clang
+# Rust
+# Install Rustup: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Python
+# Curl https://www.python.org/ftp/python/3.12.2/Python-3.12.2.tgz
+# Build
