a978b0de9378d16385b5bc4f42d7a34d348466c2
Owsley <owsley.wood@gmail.com>
2024-02-28 08:01:18 -0800

    Added VS Code to default programs.

--- install.sh
+++ install.sh
@@ -21,6 +21,13 @@ cd ..
 # Install Rustup: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl --proto '=http' --tlsv1.2 -sSL https://sh.rustup.rs | sh
 
+# Emacs
+
+# VS Code
+curl -lf "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o ~/programs/linux-deb-x64.deb
+
+apt install -y ~/programs/linux-deb-x64.deb
+
 # Configure Shell
 
 #	Install Oh my Zsh
